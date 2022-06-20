eb2fbf994254f4dc1840da305a9f5e5478aa4154
roberthinch <robert.hinch@gmail.com>
2022-06-20 17:04:25 +0100

    Add simlest example of flat r and dist

---
+++ examples/flat_r_dist.R
@@ -0,0 +1,8 @@
+# 
+# Example:     flat_d_rist
+# Description: Simple example where both r(t) and the symptom-report time distribution are constant
+#
+
+library( EpiLine )
+
+simulation = symptom_report.simulator()
