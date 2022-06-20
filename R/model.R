2bad5f1a26ad4c68e84555e6ab506a87a7a7aaf1
roberthinch <robert.hinch@gmail.com>
2022-06-20 11:59:36 +0100

    Add simple getter for complied stan model

---
+++ R/model.R
@@ -0,0 +1,8 @@
+##################################################################
+# Descrption: model.symptom_report.stan 
+#
+# Stan model object for 
+###################################################################
+model.symptom_report.stan = function() {
+  return( stanmodels$symptom_report_model )
+}
