2910826233c333b00ad7edcf5718a188302ede0c
roberthinch <robert.hinch@gmail.com>
2022-06-21 14:13:50 +0100

    Add mean/var/skewness for Johnson SU

--- R/distribution.R
+++ R/distribution.R
@@ -15,4 +15,36 @@
 #  Description: random variable of Johnson SU distribtuion
 ##################################################################
 .rjsu <- function( n, xi, lambda, gamma, delta ) 
-  return( xi + lambda * sinh( ( runif(n) -gamma) / delta))
+  return( xi + lambda * sinh( ( rnorm(n) -gamma) / delta))
+
+##################################################################
+#  Name: .jsu.mean
+#
+#  Description: mean of the Johnson SU distribution
+##################################################################
+.jsu.mean <- function( xi, lambda, gamma, delta ) {
+  omega <- exp(1/delta^2);
+  return( xi - lambda *sqrt(omega) * sinh( gamma / delta ) )
+}
+
+##################################################################
+#  Name: .jsu.var
+#
+#  Description: variance of the Johnson SU distribution
+##################################################################
+.jsu.var <- function( xi, lambda, gamma, delta ) {
+  omega <- exp(1/delta^2);
+  return( 0.5 * (lambda^2 )* (omega - 1)*(omega*cosh(2*gamma/delta) + 1 ) )
+}
+
+##################################################################
+#  Name: .jsu.skewness
+#
+#  Description: variance of the Johnson SU distribution
+##################################################################
+.jsu.skewness <- function( xi, lambda, gamma, delta ) {
+  omega <- exp(1/delta^2);
+  skew  <- -0.25*(lambda^3)*sqrt(omega)*((omega-1)^2)*(omega*(omega+2)*sinh(3*gamma/delta)+3*sinh(gamma/delta))
+  return( skew / .jsu.var( xi, lambda, gamma, delta )^{3/2})
+}
+
