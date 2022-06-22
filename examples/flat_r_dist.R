90346236b29bb612e671b229a46ecc0bb3778217
roberthinch <robert.hinch@gmail.com>
2022-06-22 11:45:07 +0100

    Add option of adding the simulation data to r(t) and symptom_report.dist plots

--- examples/flat_r_dist.R
+++ examples/flat_r_dist.R
@@ -16,6 +16,6 @@ report_date <- as.Date("2022-04-01")
 
 fit <- symptom_report.fit( reported, ll_symptom, ll_report, report_date = report_date )
 fit$plot.symptoms( simulation = simulation ) 
-fit$plot.r()
-fit$plot.symptom_report.dist()
+fit$plot.r( simulation = simulation )
+fit$plot.symptom_report.dist( simulation = simulation )
 
