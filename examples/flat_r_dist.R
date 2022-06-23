3f8fdb23b502aa9a8b7e4cb89331c0089a8b4fed
roberthinch <robert.hinch@gmail.com>
2022-06-23 10:12:38 +0100

    Show how to change the MCMC parameters in the examples

--- examples/flat_r_dist.R
+++ examples/flat_r_dist.R
@@ -14,7 +14,11 @@ ll_report   <- simulation$linelist$report
 ll_symptom  <- simulation$linelist$symptom
 report_date <- as.Date("2022-04-01")
 
-fit <- symptom_report.fit( reported, ll_symptom, ll_report, report_date = report_date )
+# fit using model
+mcmc_n_samples <- 100    
+mcmc_n_chains  <- 1
+fit <- symptom_report.fit( reported, ll_symptom, ll_report, report_date = report_date, 
+                           mcmc_n_samples = mcmc_n_samples, mcmc_n_chains = mcmc_n_chains )
 fit$plot.symptoms( simulation = simulation ) 
 fit$plot.r( simulation = simulation )
 fit$plot.symptom_report.dist( simulation = simulation )
