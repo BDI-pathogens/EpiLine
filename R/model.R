1da6705a2db4b6a9d6bbb756883d406097864a58
roberthinch <robert.hinch@gmail.com>
2022-09-27 12:03:46 +0100

    Allow mode.symptom_report.stan to return different model depending on required distribution

--- R/model.R
+++ R/model.R
@@ -4,6 +4,11 @@
 #  Description: Gets the Stan model object for the symptom_report model
 # 
 ###################################################################
-model.symptom_report.stan <- function() {
-  return( stanmodels$symptom_report_model )
+model.symptom_report.stan <- function( distribution = distribution.johnson_su ) {
+  if( distribution == distribution.johnson_su )
+    return( stanmodels$symptom_report_model )
+  if( distribution == distribution.gamma )
+    return( stanmodels$symptom_report_model_gamma )
+  
+  stop( sprintf( "Distribution %s not implemented", distribution ) )
 }
